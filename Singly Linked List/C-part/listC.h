2ebc0553573d21b3f7f48dc88d84bb2b41cd95d0
ivek81cro <ivek81cro@gmail.com>
2019-08-07 19:37:34 +0200

    fix

--- Singly Linked List/C-part/listC.h
+++ Singly Linked List/C-part/listC.h
@@ -9,7 +9,7 @@ void list_delete(list_t list); //delete list
 void list_push(list_t list, list_element_t elem);//add element to list (push)
 void list_insert_at(list_t list, list_element_t elem, int pos);//insert at position pos
 void list_print(list_t list);//print all list elements
-void list_delete_element(list_t list, int index);//delete element
+int list_delete_element(list_t list, int index);//delete element
 int list_ifExists_element(list_t list, list_element_t elem);//check if exists element, returns 1 if true
 
 #endif // !LISTC_H
